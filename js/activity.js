ccd4c2b12ab29040f3c114894a0ecc4af8b7f495
ac-mmi <aman.chadha.mmi@gmail.com>
2025-07-11 00:26:49 +0530

    testing

--- js/activity.js
+++ js/activity.js
@@ -411,7 +411,7 @@ class Activity {
 
             this.searchWidget = docById("search");
             this.searchWidget.style.visibility = "hidden";
-            this.searchWidget.placeholder = _("Search for blocks testing 78910111213");
+            this.searchWidget.placeholder = _("Search for blocks testing");
 
             this.helpfulSearchWidget = document.createElement("input");
             this.helpfulSearchWidget.setAttribute("id", "helpfulSearch");
